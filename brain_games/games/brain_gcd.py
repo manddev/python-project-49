b87050aaebbc53c9eef2a2a37b265707dbbf377f
andrew.m <malanddev@yandex.ru>
2025-02-08 23:17:14 +0300

    fix gcd and description

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -1,22 +1,18 @@
+import math
 from random import randint
 from brain_games.games_core import make_game
 
-def get_greater_common_divisor(num1, num2):
-    if (num2 > 0):
-        divisor = num1 % num2
-        return get_greater_common_divisor(num2, divisor)
-    return abs(num1)
 
-description = 'Find the greatest common divisor of given numbers.'
+DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 def make_game_data():
     current_number1 = randint(1, 100)
     current_number2 = randint(1, 100)
     question = f'{current_number1} {current_number2}'
-    correct_answer = str(get_greater_common_divisor(current_number1, current_number2))
+    correct_answer = str(math.gcd(current_number1, current_number2))
 
     return (question, correct_answer)
 
 def start_game():
-    make_game(description, make_game_data)
+    make_game(DESCRIPTION, make_game_data)
 
