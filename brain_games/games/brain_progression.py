5f602537b459c292cc1078e0be3fd9e1bef86645
andrew.m <malanddev@yandex.ru>
2025-02-10 11:15:59 +0300

    update constant variables

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -1,20 +1,24 @@
 from random import randint
 from brain_games.games_core import make_game
 
-description = 'What number is missing in the progression?'
-progression_length = 10
+MIN_VALUE = 1
+MAX_VALUE = 100
+
+DESCRIPTION = 'What number is missing in the progression?'
+PROGRESSION_LENGTH = 10
 
 def make_game_data():
+    
     progression = []
-    first_element = randint(1, 100)
-    progression_step = randint(1, 100)
+    first_element = randint(MIN_VALUE, MAX_VALUE)
+    progression_step = randint(MIN_VALUE, MAX_VALUE)
     progression.append(first_element)
 
-    for i in range(0, progression_length - 1):
+    for i in range(0, PROGRESSION_LENGTH - 1):
         next_element = progression[i] + progression_step
         progression.append(next_element)
 
-    hidden_element_index = randint(1, progression_length - 1)
+    hidden_element_index = randint(1, PROGRESSION_LENGTH - 1)
     correct_answer = str(progression[hidden_element_index])
     progression[hidden_element_index] = '..'
     question = ' '.join(str(e) for e in progression)
@@ -22,7 +26,7 @@ def make_game_data():
     return (question, correct_answer)
 
 def start_game():
-    make_game(description, make_game_data)
+    make_game(DESCRIPTION, make_game_data)
 
 
 
