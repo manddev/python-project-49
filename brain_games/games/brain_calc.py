9eb9497a08221485a2cc0f33b6a558872bdcfb64
andrew.m <malanddev@yandex.ru>
2025-02-10 11:17:27 +0300

    update constant variables

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,6 +1,8 @@
 from random import randint
-from brain_games.games_core import make_game
 
+
+MIN_VALUE = 1
+MAX_VALUE = 100
 
 def calculate_expression(num1, num2, operation):
     match operation:
@@ -15,17 +17,17 @@ def calculate_expression(num1, num2, operation):
         
 
 
-description = 'What is the result of the expression?'
+DESCRIPTION = 'What is the result of the expression?'
 
 def make_game_data():
-    operations = ['*', "+", "-"]
+    operations = ["*", "+", "-"]
     current_operation = operations[randint(0, len(operations) - 1)]
-    num1 = randint(0, 100)
-    num2 = randint(0, 100)
+    num1 = randint(MIN_VALUE, MAX_VALUE)
+    num2 = randint(MIN_VALUE, MAX_VALUE)
     question = f'{num1} {current_operation} {num2}'
     correct_answer = str(calculate_expression(num1, num2, current_operation))
 
     return (question, correct_answer)
 
 def start_game():
-    make_game(description, make_game_data)
+    make_game(DESCRIPTION, make_game_data)
