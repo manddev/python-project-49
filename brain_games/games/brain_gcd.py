10e35fd73070fa6844bfee3609cba6e716927e98
andrew.m <malanddev@yandex.ru>
2025-02-11 06:28:29 +0300

    fix variables

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -2,12 +2,15 @@ import math
 from random import randint
 from brain_games.games_core import make_game
 
+RANDINT_MIN_VALUE = 1
+RANDINT_MAX_VALUE = 100
+
 
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 def make_game_data():
-    current_number1 = randint(1, 100)
-    current_number2 = randint(1, 100)
+    current_number1 = randint(RANDINT_MIN_VALUE, RANDINT_MAX_VALUE)
+    current_number2 = randint(RANDINT_MIN_VALUE, RANDINT_MAX_VALUE)
     question = f'{current_number1} {current_number2}'
     correct_answer = str(math.gcd(current_number1, current_number2))
 
