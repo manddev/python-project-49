d1adae043323f82a2a5e4bec6e7d32206853e0f7
andrew.m <malanddev@yandex.ru>
2025-02-11 06:24:57 +0300

    fix variables

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -1,12 +1,15 @@
 from random import randint 
 
 
-MIN_VALUE = 1
-MAX_VALUE = 100
+RANDINT_MIN_VALUE = 1
+RANDINT_MAX_VALUE = 100
 
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def is_prime(num):
+    if num < 2:
+        return False
+    
     for i in range(2, num // 2):
         if num % i == 0:
             return False
@@ -14,7 +17,7 @@ def is_prime(num):
     return True
 
 def make_game_data():
-    current_number = randint(MIN_VALUE, MAX_VALUE )
+    current_number = randint(RANDINT_MIN_VALUE, RANDINT_MAX_VALUE)
     question = f'{current_number}'
     correct_answer = 'yes' if is_prime(current_number) else 'no'
 
