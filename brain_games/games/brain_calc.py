e03187bf52e4d95daf3e170d7264c0d467d54266
andrew.m <malanddev@yandex.ru>
2025-02-11 06:29:42 +0300

    fix variables

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,8 +1,8 @@
 from random import randint
 
 
-MIN_VALUE = 1
-MAX_VALUE = 100
+RANDINT_MIN_VALUE = 1
+RANDINT_MAX_VALUE = 100
 
 def calculate_expression(num1, num2, operation):
     match operation:
@@ -22,8 +22,8 @@ DESCRIPTION = 'What is the result of the expression?'
 def make_game_data():
     operations = ["*", "+", "-"]
     current_operation = operations[randint(0, len(operations) - 1)]
-    num1 = randint(MIN_VALUE, MAX_VALUE)
-    num2 = randint(MIN_VALUE, MAX_VALUE)
+    num1 = randint(RANDINT_MIN_VALUE, RANDINT_MAX_VALUE)
+    num2 = randint(RANDINT_MIN_VALUE, RANDINT_MAX_VALUE)
     question = f'{num1} {current_operation} {num2}'
     correct_answer = str(calculate_expression(num1, num2, current_operation))
 
