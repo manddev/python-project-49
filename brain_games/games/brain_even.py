2c4d24d370fbe5178c2a91714ae1221a5d232905
andrew.m <malanddev@yandex.ru>
2025-02-11 06:31:27 +0300

    fix variables

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,22 +1,23 @@
 from random import randint
 from brain_games.games_core import make_game
 
-
+RANDINT_MIN_VALUE = 1
+RANDINT_MAX_VALUE = 100
 
 def is_even(num):
     return num % 2 == 0
 
-description = 'Answer "yes" if the number is even, otherwise answer "no".'
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def make_game_data():
-    current_number = randint(1, 100)
+    current_number = randint(RANDINT_MIN_VALUE, RANDINT_MAX_VALUE)
     question = f'{current_number}'
     correct_answer = 'yes' if is_even(current_number) else 'no'
 
     return (question, correct_answer)
 
 def start_game():
-    make_game(description, make_game_data)
+    make_game(DESCRIPTION, make_game_data)
     
         
 
