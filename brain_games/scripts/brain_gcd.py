4f92c040006ca24de39715f1a4ac04bf44d40bbf
andrew.m <malanddev@yandex.ru>
2025-02-15 09:29:56 +0300

    fix imports

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,6 +1,6 @@
 from brain_games.games import brain_gcd
-from brain_games.games_core import make_game
+from brain_games.games_core import run_game
 
 
 def main():
-    make_game(brain_gcd.DESCRIPTION, brain_gcd.make_game_data)
+    run_game(brain_gcd.DESCRIPTION, brain_gcd.make_game_data)
