c1f285deb1e1b7e1828e703286d35b1ca1b9e735
andrew.m <malanddev@yandex.ru>
2025-02-15 09:11:34 +0300

    fix prints

--- brain_games/games_core.py
+++ brain_games/games_core.py
@@ -6,11 +6,13 @@ ROUNDS_COUNT = 3
 def run_game(description, make_game_data):
     print('Welcome to the Brain Games!')
     user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!\n{description}')
+    print(f'Hello, {user_name}!')
+    print(description)
 
     for i in range(0, ROUNDS_COUNT):
         (question, correct_answer) = make_game_data()
-        user_answer = prompt.string(f'Question: {question}\nYour answer: ')
+        print(f'Question: {question}')
+        user_answer = prompt.string('Your answer: ')
         if correct_answer != user_answer:
             print(
                 f"'{user_answer}' is wrong answer ;(. "
